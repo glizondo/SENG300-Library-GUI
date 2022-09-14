2367ad292e4b1e136c83de3d94c21fe6ea7200a3
glizondo <80534057+glizondo@users.noreply.github.com>
2022-09-14 15:11:04 -0400

    Modified variables to the names in the books.csv file

--- src/Book.java
+++ src/Book.java
@@ -6,62 +6,66 @@ import java.util.Scanner;
 
 public class Book {
 
-	int bookId;
-	int bookGoodreadsId;
-	int bestBookId;
-	int workId;
-	int booksCount;
-	String ISBN;
-	String ISBN13;
+	int book_id;
+	int goodreads_book_id;
+	int best_book_id;
+	int work_id;
+	int books_count;
+	String isbn;
+	String isbn13;
 	String authors;
-	double originalPublicationYear;
-	String originalTitle;
+	double original_publication_year;
+	String original_title;
 	String title;
-	String languageCode;
-	double averageRating;
-	int ratingsCount;
-	int workRatingsCount;
-	int workTextReviewsCount;
-	int ratings1;
-	int ratings2;
-	int ratings3;
-	int ratings4;
-	int ratings5;
-	String imageURL;
-	String smallImageURL;
+	String language_code;
+	double average_rating;
+	int ratings_count;
+	int work_ratings_count;
+	int work_text_reviews_count;
+	int ratings_1;
+	int ratings_2;
+	int ratings_3;
+	int ratings_4;
+	int ratings_5;
+	String image_url;
+	String small_image_url;
 
 	FileInputStream fileReader;
 	Scanner reader;
 	ListCreator list;
 
-	public Book(int bookId, int bookGoodreadsId, int bestBookId, int workId, int booksCount, String iSBN, String iSBN13,
-			String authors, double originalPublicationYear, String originalTitle, String title, String languageCode,
-			double averageRating, int ratingsCount, int workRatingsCount, int workTextReviewsCount, int ratings1,
-			int ratings2, int ratings3, int ratings4, int ratings5, String imageURL, String smallImageURL) {
+	public Book(int book_id, int goodreads_book_id, int best_book_id, int work_id, int books_count, String isbn,
+			String isbn13, String authors, double original_publication_year, String original_title, String title,
+			String language_code, double average_rating, int ratings_count, int work_ratings_count,
+			int work_text_reviews_count, int ratings_1, int ratings_2, int ratings_3, int ratings_4, int ratings_5,
+			String image_url, String small_image_url, FileInputStream fileReader, Scanner reader, ListCreator list) {
 		super();
-		this.bookId = bookId;
-		this.bookGoodreadsId = bookGoodreadsId;
-		this.bestBookId = bestBookId;
-		this.workId = workId;
-		this.booksCount = booksCount;
-		ISBN = iSBN;
-		ISBN13 = iSBN13;
+		this.book_id = book_id;
+		this.goodreads_book_id = goodreads_book_id;
+		this.best_book_id = best_book_id;
+		this.work_id = work_id;
+		this.books_count = books_count;
+		this.isbn = isbn;
+		this.isbn13 = isbn13;
 		this.authors = authors;
-		this.originalPublicationYear = originalPublicationYear;
-		this.originalTitle = originalTitle;
+		this.original_publication_year = original_publication_year;
+		this.original_title = original_title;
 		this.title = title;
-		this.languageCode = languageCode;
-		this.averageRating = averageRating;
-		this.ratingsCount = ratingsCount;
-		this.workRatingsCount = workRatingsCount;
-		this.workTextReviewsCount = workTextReviewsCount;
-		this.ratings1 = ratings1;
-		this.ratings2 = ratings2;
-		this.ratings3 = ratings3;
-		this.ratings4 = ratings4;
-		this.ratings5 = ratings5;
-		this.imageURL = imageURL;
-		this.smallImageURL = smallImageURL;
+		this.language_code = language_code;
+		this.average_rating = average_rating;
+		this.ratings_count = ratings_count;
+		this.work_ratings_count = work_ratings_count;
+		this.work_text_reviews_count = work_text_reviews_count;
+		this.ratings_1 = ratings_1;
+		this.ratings_2 = ratings_2;
+		this.ratings_3 = ratings_3;
+		this.ratings_4 = ratings_4;
+		this.ratings_5 = ratings_5;
+		this.image_url = image_url;
+		this.small_image_url = small_image_url;
+		this.fileReader = fileReader;
+		this.reader = reader;
+		this.list = list;
 	}
 
 //	public Book() {
