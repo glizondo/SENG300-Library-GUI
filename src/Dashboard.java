7470ba211681c0c893703c1d078fe78676c3d4a8
Mark <hamilm25@ferris.edu>
2022-09-15 23:54:23 -0400

    save Libreria files

--- src/Dashboard.java
+++ src/Dashboard.java
@@ -1,10 +1,192 @@
+import java.awt.Color;
 import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.List;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 
 public class Dashboard {
 
 	private JFrame frame;
+	private JMenuBar menuBar;                                              //Create JFrame
+	private JMenu fileMenu;                                                //Created file menu 
+	private JMenu bookMenu;                                                //Created book menu
+	private JMenu searchMenu;                                              //Created search menu
+	private JMenu printMenu;                                               //Created print menu
+	private JMenuItem exit;
+	private JMenuItem add;
+	private JMenuItem purchase;
+	private JMenuItem update;
+	private JMenuItem remove;
+	private JMenuItem searchID;
+	private JMenuItem searchAuthor;
+	private JMenuItem printAll;
+	private JPanel panel;
+	private JLabel welcome;
+	private JScrollPane scrollPane;
+	private JTextArea text;
+	private ListCreator library = new ListCreator();
+	
+
+	/**
+	 * Create the application.
+	 */
+	public Dashboard() {
+		initialize();
+		loadBooks();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		createMenuBar();
+	      
+	      panel = new JPanel();
+	      panel.setBackground(new Color(0,0,155));
+	      welcome = new JLabel("Welcome to the Book Library App");
+	      welcome.setForeground(Color.cyan);
+	      welcome.setFont(new Font("SansSerif", Font.BOLD,20));
+	      panel.add(welcome);
+	      text = new JTextArea(15, 30);
+	      text.setFont(new Font("SansSerif", Font.PLAIN,16));
+	      scrollPane = new JScrollPane(text);
+	      scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+	      text.setLineWrap(true);                                                      //Sets the line-wrapping policy of the text area
+	      text.setWrapStyleWord(true);                                                 //Sets the style of wrapping used if the text area is wrapping lines
+	      panel.add(scrollPane);
+	      frame.add(panel);
+
+		frame.setSize(500, 500);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	
+	public void createMenuBar()  {
+		menuBar = new JMenuBar();
+	      menuBar.setBackground(new Color(0, 0, 255));
+	      menuBar.setForeground(Color.white);
+	      fileMenu = new JMenu("File");
+	      bookMenu = new JMenu("Book");
+	      searchMenu = new JMenu("Search");
+	      printMenu = new JMenu("Print");
+	      fileMenu.setForeground(Color.white);
+	      bookMenu.setForeground(Color.white);
+	      searchMenu.setForeground(Color.white);
+	      printMenu.setForeground(Color.white);
+	      fileMenu.setFont(new Font("SansSerif", Font.BOLD,16));
+	      bookMenu.setFont(new Font("SansSerif", Font.BOLD,16));
+	      searchMenu.setFont(new Font("SansSerif", Font.BOLD,16));
+	      printMenu.setFont(new Font("SansSerif", Font.BOLD,16));
+	      
+	      menuBar.add(fileMenu);
+	      menuBar.add(bookMenu);
+	      menuBar.add(searchMenu);
+	      menuBar.add(printMenu);
+	      
+	      exit = new JMenuItem("Exit");
+	      add = new JMenuItem("Add");
+	      purchase = new JMenuItem("Purchase");
+	      update = new JMenuItem("Update");
+	      remove = new JMenuItem("Remove");
+	      searchID = new JMenuItem("ISBN");
+	      searchAuthor = new JMenuItem("Author");
+	      printAll= new JMenuItem("All");
+	      
+	      exit.setBackground(Color.cyan);
+	      exit.setForeground(Color.blue);
+	      exit.setFont(new Font("SansSerif", Font.BOLD,16));
+	      add.setBackground(Color.CYAN);
+	      add.setForeground(Color.blue);
+	      add.setFont(new Font("SansSerif", Font.BOLD,16));
+	      purchase.setBackground(Color.CYAN);
+	      purchase.setForeground(Color.blue);
+	      purchase.setFont(new Font("SansSerif", Font.BOLD,16));
+	      update.setBackground(Color.CYAN);
+	      update.setForeground(Color.blue);
+	      update.setFont(new Font("SansSerif", Font.BOLD,16));
+	      remove.setBackground(Color.CYAN);
+	      remove.setForeground(Color.blue);
+	      remove.setFont(new Font("SansSerif", Font.BOLD,16));
+	      searchID.setBackground(Color.CYAN);
+	      searchID.setForeground(Color.blue);
+	      searchID.setFont(new Font("SansSerif", Font.BOLD,16));
+	      searchAuthor.setBackground(Color.CYAN);
+	      searchAuthor.setForeground(Color.blue);
+	      searchAuthor.setFont(new Font("SansSerif", Font.BOLD,16));
+	      printAll.setBackground(Color.CYAN);
+	      printAll.setForeground(Color.blue);
+	      printAll.setFont(new Font("SansSerif", Font.BOLD,16));
+	      
+	      fileMenu.add(exit);
+	      bookMenu.add(add);
+	      bookMenu.add(purchase);
+	      bookMenu.add(update);
+	      bookMenu.add(remove);
+	      searchMenu.add(searchID);
+	      searchMenu.add(searchAuthor);
+	      printMenu.add(printAll);
+	      
+	      frame.setJMenuBar(menuBar);
+	      
+	      exit.addActionListener(new ExitListener());
+	      //add.addActionListener(new AddBookListener());
+	      //purchase.addActionListener(new PurchaseBookListener());
+	      //update.addActionListener(new UpdateBookListener());
+	      //remove.addActionListener(new RemoveBookListener());
+	      searchID.addActionListener(new SearchIDListener());
+	      //searchAuthor.addActionListener(new SearchAuthorListener());
+	      //printAll.addActionListener(new PrintAllListener());
+	      
+	}
+	private void loadBooks()  {
+		library.readFile();
+		List<Book> books = library.getList();
+		String output = "";
+		for(Book b:books)  {
+			output += b + "\n";
+		}
+		text.setText(output);
+	}
+	private class ExitListener implements ActionListener  {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			System.exit(0);
+		}
+		
+	}
+	private class SearchIDListener implements ActionListener  {
+		
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			try  {
+				String ID = JOptionPane.showInputDialog("Enter ISBN");
+				int ISBN = Integer.parseInt(ID.trim());
+				/*Book book = library.searchBook(ISBN);
+				if(book != null)  {
+					text.setText(book.toString());
+				}
+				else  {
+					JOptionPane.showMessageDialog(null, "There is no book with that ISBN");
+				}*/
+			}
+			catch(Exception ex) {
+				JOptionPane.showMessageDialog(null, "Please enter a valid ISBN");
+			}
+		}
+		
+	}
 
 	/**
 	 * Launch the application.
@@ -21,21 +203,4 @@ public class Dashboard {
 			}
 		});
 	}
-
-	/**
-	 * Create the application.
-	 */
-	public Dashboard() {
-		initialize();
-	}
-
-	/**
-	 * Initialize the contents of the frame.
-	 */
-	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	}
-
 }
--- src/ListCreator.java
+++ src/ListCreator.java
@@ -27,43 +27,44 @@ public class ListCreator {
 	public void readFile()  {
 		
 		try  {
-			FileInputStream fileReader = new FileInputStream("books4.csv");
+			FileInputStream fileReader = new FileInputStream("books.csv");
 			Scanner input = new Scanner(fileReader);
 			input.nextLine();
 			while(input.hasNext())  {
 				String line = input.nextLine();
 				System.out.println(line);
 				String[] fields = line.split(",");
-				int bookId = Integer.parseInt(fields[0]);
-				int bookGoodreadsId = Integer.parseInt(fields[1]);
-				int bestBookId = Integer.parseInt(fields[2]);
-				int workId = Integer.parseInt(fields[3]);
-				int booksCount = Integer.parseInt(fields[4]);
-				String iSBN = fields[5];
-				String iSBN13 = fields[6];
-				String authors = fields[7];
-				double originalPublicationYear = Double.parseDouble(fields[8]);
-				String originalTitle = fields[9];
-				String title = fields[10];
-				String languageCode = fields[11];
-				double averageRating = Double.parseDouble(fields[12]);
-				int ratingsCount = Integer.parseInt(fields[13]);
-				int workRatingsCount = Integer.parseInt(fields[14]);
-				int workTextReviewsCount = Integer.parseInt(fields[15]);
-				int ratings1 = Integer.parseInt(fields[16]);
-				int ratings2 = Integer.parseInt(fields[17]);
-				int ratings3 = Integer.parseInt(fields[18]);
-				int ratings4 = Integer.parseInt(fields[19]);
-				int ratings5 = Integer.parseInt(fields[20]);
-				String imageURL = fields[21];
-				String smallImageURL = fields[22];
-		
-				Book book = new Book(bookId, bookGoodreadsId, bestBookId, workId, booksCount, iSBN, iSBN13,
-						authors, originalPublicationYear, originalTitle, title, languageCode,
-						averageRating, ratingsCount, workRatingsCount, workTextReviewsCount, ratings1,
-						ratings2, ratings3, ratings4, ratings5, imageURL, smallImageURL);
-				list.add(book);
-				
+				if(fields.length==23) {
+					int bookId = Integer.parseInt(fields[0]);
+					int bookGoodreadsId = Integer.parseInt(fields[1]);
+					int bestBookId = Integer.parseInt(fields[2]);
+					int workId = Integer.parseInt(fields[3]);
+					int booksCount = Integer.parseInt(fields[4]);
+					String iSBN = fields[5];
+					String iSBN13 = fields[6];
+					String authors = fields[7];
+					double originalPublicationYear = Double.parseDouble(fields[8]);
+					String originalTitle = fields[9];
+					String title = fields[10];
+					String languageCode = fields[11];
+					double averageRating = Double.parseDouble(fields[12]);
+					int ratingsCount = Integer.parseInt(fields[13]);
+					int workRatingsCount = Integer.parseInt(fields[14]);
+					int workTextReviewsCount = Integer.parseInt(fields[15]);
+					int ratings1 = Integer.parseInt(fields[16]);
+					int ratings2 = Integer.parseInt(fields[17]);
+					int ratings3 = Integer.parseInt(fields[18]);
+					int ratings4 = Integer.parseInt(fields[19]);
+					int ratings5 = Integer.parseInt(fields[20]);
+					String imageURL = fields[21];
+					String smallImageURL = fields[22];
+			
+					Book book = new Book(bookId, bookGoodreadsId, bestBookId, workId, booksCount, iSBN, iSBN13,
+							authors, originalPublicationYear, originalTitle, title, languageCode,
+							averageRating, ratingsCount, workRatingsCount, workTextReviewsCount, ratings1,
+							ratings2, ratings3, ratings4, ratings5, imageURL, smallImageURL);
+					list.add(book);
+				}
 			}
 		}
 
@@ -78,6 +79,9 @@ public class ListCreator {
 		//@SuppressWarnings("rawtypes")
 		lce.readFile();
 		List<Book> list = lce.getList();
-		System.out.println(list);
+		for(Book b:list)  {
+			System.out.println(b);
+		}
+		
 	}
 }
